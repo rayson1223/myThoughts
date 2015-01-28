9d2eed7bab0f086db291be30daa2264d467520f9
Rayson <raysonlcp1223@gmail.com>
2015-01-28 21:59:32 +0800

    FG: Added Default user data + Remove warnings from Rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    username                "tester"
+    email                   "tester@gmail.com"
+    password                "123123123"
+    password_confirmation   "123123123"
   end
-
 end
