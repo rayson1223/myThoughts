5286bada3af81eb7f55ec16450d6a81d5f27c818
Rayson <raysonlcp1223@gmail.com>
2015-01-29 10:21:55 +0800

    Rspec: User Model Test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#new' do
+
+    before :each do
+      @user = FactoryGirl.build(:user)
+    end
+
+    it "should not be valid without an email" do
+      @user.email = nil
+      expect(@user.save!).to be_falsey
+    end
+
+    it "should not be valid without a username" do
+      @user.username = nil
+      expect(@user.save!).to be_falsey
+    end
+
+    it "should not be valid without a password" do
+      @user.password = nil
+      expect(@user.save!).to be_falsey
+    end
+
+  end
 end
