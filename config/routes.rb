6ab2149fecbb6a72d64c3434e32a626716bad073
Rayson <raysonlcp1223@gmail.com>
2015-01-29 20:30:59 +0800

    Destroy Scaffold + Generate Home Controller: Set up index home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :homes
-
   devise_for :users
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/factories/homes.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :home do
-    
-  end
-
-end
