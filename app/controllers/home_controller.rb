140ab37ff3ffd33b3998e7f9ef8d5900c9d07665
Rayson <raysonlcp1223@gmail.com>
2015-02-02 04:11:26 +0800

    Able to delete thoughts now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,12 @@ class HomeController < ApplicationController
   def index
     if (!user_signed_in?)
       redirect_to new_user_session_path
-    end
-    @thought ||= Thought.new
-    @allThought = Thought.where(user_id: current_user.id).order("updated_at DESC")
+    else
+      @thought ||= Thought.new
+      @allThought = Thought.where(user_id: current_user.id).order("updated_at DESC")
 
-    @allTags = getUserTags! current_user.id
+      @allTags = getUserTags! current_user.id
+    end
   end
 
   def search
--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -74,7 +74,7 @@ class ThoughtsController < ApplicationController
   def destroy
     @thought.destroy
     respond_to do |format|
-      format.html { redirect_to thoughts_url, notice: 'Thought was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Thought was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
