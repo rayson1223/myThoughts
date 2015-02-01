cbeb3d37bdc7b0425248a5c84977cc24ac1fd6c8
Rayson <raysonlcp1223@gmail.com>
2015-02-01 23:27:46 +0800

    Generate Thought migrations

--- db/migrate/20150201151504_create_thoughts.rb
+++ db/migrate/20150201151504_create_thoughts.rb
@@ -1,7 +1,8 @@
 class CreateThoughts < ActiveRecord::Migration
   def change
     create_table :thoughts do |t|
-
+      t.integer :user_id
+      t.text :content,              null: false, default: ""
       t.timestamps
     end
   end
