c5e455f1e99a2ad233e4c90facc7083c71dbad69
Rayson <raysonlcp1223@gmail.com>
2015-02-02 01:16:37 +0800

    Inititate Thought UI DONE

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
+
   def index
     if (!user_signed_in?)
       redirect_to new_user_session_path
     end
     @thought ||= Thought.new
+    @allThought = Thought.where(user_id: current_user.id)
+
   end
 end
