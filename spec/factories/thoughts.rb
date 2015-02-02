a2f6a166578bd36a65b2d484177595caae2f21e0
Rayson <raysonlcp1223@gmail.com>
2015-02-02 10:17:45 +0800

    Model Rspec: Thought Spec

--- spec/factories/thoughts.rb
+++ spec/factories/thoughts.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :thought do
-    
+    user_id     1
+    content     "#TEST thought!"
   end
 
 end
--- spec/models/thought_spec.rb
+++ spec/models/thought_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Thought, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Thoughts#New" do
+    it "has a valid factory" do
+      expect(FactoryGirl.create(:thought)).to be_valid
+    end
+
+    it "invalid without user" do
+      expect(FactoryGirl.build(:thought, user_id: nil)).to be_invalid
+    end
+
+    it "invalid without user" do
+      user = FactoryGirl.create(:user)
+      expect(FactoryGirl.build(:thought, {user_id: user.id, content: nil })).to be_invalid
+    end
+    # it "Should increment count by 1" do
+    #   count = Thought.all.length
+    #   t1 = FactoryGirl.create(:thought)
+
+    #   expect{Thought.count}.to change{Thought.count}.from(count).to(Thought.count)
+    # end
+  end
 end
