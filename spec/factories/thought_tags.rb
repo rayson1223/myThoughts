3e90ca0ae682a6cfad22c262a18dacb2c5c5d1c0
Rayson <raysonlcp1223@gmail.com>
2015-02-02 11:45:46 +0800

    Rspec Model: Thought Tag Test Case

--- spec/factories/thought_tags.rb
+++ spec/factories/thought_tags.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :thought_tag do
-    
+    thought_id    1
+    hash_tag_id   1
   end
 
 end
--- spec/models/thought_tag_spec.rb
+++ spec/models/thought_tag_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ThoughtTag, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#new' do
+
+    it "has a valid factory" do
+      expect(FactoryGirl.create(:thought_tag)).to be_valid
+    end
+
+    it "should not be valid without hash_tag_id" do
+      expect(FactoryGirl.build(:thought_tag, {thought_id: 1})).to be_invalid
+    end
+
+    it "should not be valid without thought_id" do
+      expect(FactoryGirl.build(:thought_tag, {hash_tag_id: 1})).to be_invalid
+    end
+
+  end
 end
