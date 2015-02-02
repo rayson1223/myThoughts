e3fa33bb5104d25b2a152a72ef0fdb5d864894da
Rayson <raysonlcp1223@gmail.com>
2015-02-02 10:20:17 +0800

    Rspec Model: Thought model valid

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -2,4 +2,7 @@ class Thought < ActiveRecord::Base
   belongs_to :user
   has_many :thought_tags
   has_many :hash_tags, through: :thought_tags
+
+  validates :user_id, presence: true
+  validates :content, presence: true
 end
